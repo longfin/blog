d471d5ac618504af1de265685a99dffd699179a3
Swen Mun <longfinfunnel@gmail.com>
2018-01-06 22:41:23 +0900

    Circle CI 설정

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git config --global user.email "${GIT_EMAIL}"
+git config --global user.name "${GIT_NAME}"
+
+rm -rf build
+git clone git@github.com:longfin/longfin.github.io.git build
+cp -rf resources/public/* build
+cd build
+git add *
+git commit -a -m "build"
+git push -u origin master
+cd ..
